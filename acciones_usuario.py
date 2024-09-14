cf58ad18bf0772ca89d77e5ec6d02ee4f6d92d26
FranciscoMessina <francisco@messina.ws>
2024-09-14 15:52:48 -0300

    agregado opcion de cantar truco para el usuario

--- acciones_usuario.py
+++ acciones_usuario.py
@@ -6,4 +6,7 @@ def pedir_accion_usuario(cartas, partida, numero_ronda):
     for carta in cartas:
         opciones.append([f"Jugar {formatear_carta(carta)}", {"carta": carta, "accion": "jugar_carta"}])
 
+    if partida['mano_actual']['truco'] is None:
+        opciones.append(["Cantar truco", {"accion": "cantar_truco"}])
+
     return pedir_eleccion(opciones)
