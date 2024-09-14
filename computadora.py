4de4410ded1516bdb2cf36066b82e2cfc62206f4
FranciscoMessina <francisco@messina.ws>
2024-09-14 15:53:01 -0300

    agregado  accion de responder al truco de la computadora

--- computadora.py
+++ computadora.py
@@ -1,5 +1,7 @@
 from random import choice
 
+from mazo import obtener_poder
+
 
 def actuar_computadora(cartas, partida, numero_ronda):
     carta_random = choice(cartas)
@@ -12,14 +14,16 @@ def actuar_computadora(cartas, partida, numero_ronda):
 
 def responder_a_usuario(accion_usuario, cartas_computadora, partida, numero_ronda):
     if accion_usuario['accion'] == 'jugar_carta':
-
         return responder_a_carta(accion_usuario['carta'], cartas_computadora, partida, numero_ronda)
+    elif accion_usuario['accion'] == 'cantar_truco':
+        return choice([{"accion": "aceptar"}, {"accion": "rechazar"}])
     else:
         return None
 
 
 def responder_a_carta(carta_jugada, cartas_computadora, partida, numero_ronda):
-    cartas_mas_fuertes = list(filter(lambda carta: carta_jugada[3] <= carta[3], cartas_computadora))
+    cartas_mas_fuertes = list(
+        filter(lambda carta: obtener_poder(carta_jugada) <= obtener_poder(carta), cartas_computadora))
 
     if len(cartas_mas_fuertes) > 0:
         return {"accion": "jugar_carta", "carta": cartas_mas_fuertes[0]}
