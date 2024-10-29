4f4bb0347795cf8c007c5c0fe05649b2603578a2
francopela555 <41063391+francopela555@users.noreply.github.com>
2024-10-28 21:22:56 -0300

    Update acciones_usuario.py

--- acciones_usuario.py
+++ acciones_usuario.py
@@ -1,5 +1,4 @@
 from envido import calcular_envido
-
 from utilidades import formatear_carta, pedir_eleccion, Colores, dev_print
 from variables import get_current_hand, get_user_cards, is_first_round, USUARIO, COMPUTADORA, envido_needs_answer, truco_needs_answer
 
@@ -26,16 +25,21 @@ def pedir_accion_usuario():
     elif truco_needs_answer():
         dev_print('AU- Responder a truco')
         from acciones import aceptar_truco, rechazar_truco
+        from acciones import cantar_truco
         opciones.append(["Quiero", aceptar_truco(USUARIO)])
         opciones.append(["No quiero", rechazar_truco(USUARIO)])
+        if mano_actual['truco'].get('nivel') == 1:
+            opciones.append(["Cantar Retruco", cantar_truco(USUARIO, 2)])
+        elif mano_actual['truco'].get('nivel') == 2:
+            opciones.append(["Cantar Vale cuatro", cantar_truco(USUARIO, 3)])
 
-   else:
+    else:
         dev_print('AU- Opciones normales')
+        from acciones import cantar_truco
         for carta in cartas:
             # Por cada carta en su mano agregamos la opcion de jugarla.
             from acciones import jugar_carta
             opciones.append([f"Jugar {formatear_carta(carta)}", jugar_carta(carta, USUARIO)])
-        from acciones import cantar_truco
         if mano_actual['truco'].get('activo') is False:
             # Si no se ha cantado truco aun, se le da la opcion de cantar truco
             if mano_actual['truco'].get('nivel') == 0:
@@ -44,7 +48,7 @@ def pedir_accion_usuario():
             if mano_actual['truco'].get('nivel') == 1 and mano_actual['truco'].get('cantado_por') == COMPUTADORA:
                 opciones.append(["Cantar Retruco", cantar_truco(USUARIO, 2)])
             elif mano_actual['truco'].get('nivel') == 2 and mano_actual['truco'].get('cantado_por') == COMPUTADORA:
-                opciones.append(["Cantar Vale_cuatro", cantar_truco(USUARIO, 3)])
+                opciones.append(["Cantar Vale cuatro", cantar_truco(USUARIO, 3)])
 
         if is_first_round():
             puntos_envido = calcular_envido(cartas)
